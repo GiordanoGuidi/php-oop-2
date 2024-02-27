24c75bb6caee2868f1c7d6eaedd3419cf242ed6b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-27 18:34:14 +0100

    added card-header

--- index.php
+++ index.php
@@ -48,6 +48,8 @@ var_dump($products);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
     <!--BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--FONTAWESOME-->
@@ -56,15 +58,18 @@ var_dump($products);
 
 <body>
     <div class="container">
-        <div class="row ">
+        <div class="row g-4">
             <?php foreach ($products as $product) : ?>
                 <div class="col-3">
-                    <div class="card h-100">
-                        <img src="<?= $product->image ?>" class="card-img-top img-fluid" alt="<?= $product->title ?>">
+                    <div class="card h-100 bg-warning p-2">
+                        <div class="card-header">
+                            <i class="<?= $product->label->icon ?>"></i>
+                            <span><?= $product->label->label ?></span>
+                        </div>
+                        <img src="<?= $product->image ?>" class="card-img-top" alt="<?= $product->title ?>">
                         <div class="card-body">
                             <h5 class="card-title"><?= $product->title ?></h5>
                             <p><?= $product->getPrice() ?></p>
-                            <i class="<?= $product->label->icon ?>"></i>
                         </div>
                     </div>
                 </div>
