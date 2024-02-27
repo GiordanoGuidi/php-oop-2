1b6fc59edfb32865840312a7e439e72a168fd086
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-27 18:49:29 +0100

    added background into product-cards

--- index.php
+++ index.php
@@ -48,8 +48,8 @@ var_dump($products);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--STYLE-->
     <link rel="stylesheet" href="style.css">
-
     <!--BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--FONTAWESOME-->
@@ -61,7 +61,8 @@ var_dump($products);
         <div class="row g-4">
             <?php foreach ($products as $product) : ?>
                 <div class="col-3">
-                    <div class="card h-100 bg-warning p-2">
+                    <!--Product card-->
+                    <div class="product-card card h-100 p-2 <?= $product->label->label ?>">
                         <div class="card-header">
                             <i class="<?= $product->label->icon ?>"></i>
                             <span><?= $product->label->label ?></span>
